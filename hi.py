3e1c0f2a03a6c262c42c37b2b65461beb7af7798
sefieldhouse <sarah.fieldhouse.2015@gmail.com>
2026-08-26 17:21:38 +0000

    First GitHub project, yay!

---
+++ hi.py
@@ -0,0 +1,5 @@
+userName = input("Hi, what is your name?\n ENTER NAME: ")
+
+print("Hi there " + userName + "!")
+print("Well hello there", userName + "!")
+print(f"Hi there {userName}. Nice to meet you!")
